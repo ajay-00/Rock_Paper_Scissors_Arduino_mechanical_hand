305b5666a1d251f672e27b691f06044d422ef774
Ajay Dhawan <58970472+ajay-00@users.noreply.github.com>
2020-11-19 16:41:29 -0500

    Rock paper scissors Arduino source code

---
+++ RPS.ino
@@ -0,0 +1,57 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+const int trigPin = 11;
+const int echoPin = 10;
+
+long duration; 
+int distance; 
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  pinMode (trigPin,OUTPUT);
+  pinMode (echoPin, INPUT);
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance =duration * 0.034/2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+
+  while(distance < 10){
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  }
+}
